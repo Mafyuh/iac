d674b1af196e2abc295ae12e001cd236b678d1e2
mafyuh <admin@mafyuh.dev>
2024-10-12 20:35:50 +0000

    add tofu workflow, more space for kasm

--- terraform/kasm.tf
+++ terraform/kasm.tf
@@ -36,7 +36,7 @@ resource "proxmox_virtual_environment_vm" "Kasm" {
   # VM Disk Settings
   disk {
     datastore_id = "local-lvm"
-    size         = 100
+    size         = 150
     interface    = "scsi0"
   }
 
