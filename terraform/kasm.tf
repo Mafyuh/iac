72d6bc68442ec592fa56cc6322c28833f394eb3b
Matt Reeves <admin@mafyuh.io>
2024-10-12 22:55:38 -0400

    test

--- terraform/kasm.tf
+++ terraform/kasm.tf
@@ -36,7 +36,7 @@ resource "proxmox_virtual_environment_vm" "Kasm" {
   # VM Disk Settings
   disk {
     datastore_id = "local-lvm"
-    size         = 151
+    size         = 152
     interface    = "scsi0"
   }
 
