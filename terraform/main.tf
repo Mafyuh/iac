3b67e5d304a7ae97d81783857e6ffc71dab8dd52
Mafyuh <admin@mafyuh.dev>
2025-04-06 23:59:40 -0400

    back to s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,30 +2,30 @@ terraform {
   required_version = ">= 1.0.0"
 
 
-# backend "s3" {
-#     bucket                      = "BigBuckets"
-#     region                      = "us-ashburn-1"
-#     key                         = "tf.tfstate"
-#     skip_region_validation      = true
-#     skip_credentials_validation = true
-#     skip_requesting_account_id  = true
-#     use_path_style              = true
-#     skip_s3_checksum            = true
-#     skip_metadata_api_check     = true
-#     endpoints = {
-#       s3 = var.s3_endpoint
-#     }
-#   }
-# }
-backend "remote" {
-    hostname = "mafyuh.scalr.io"
-    organization = "Environment-A"
-
-    workspaces {
-      name = "IaC"
+backend "s3" {
+    bucket                      = "BigBuckets"
+    region                      = "us-ashburn-1"
+    key                         = "tf.tfstate"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
+    skip_s3_checksum            = true
+    skip_metadata_api_check     = true
+    endpoints = {
+      s3 = var.s3_endpoint
     }
   }
 }
+# backend "remote" {
+#     hostname = "mafyuh.scalr.io"
+#     organization = "Environment-A"
+
+#     workspaces {
+#       name = "IaC"
+#     }
+#   }
+# }
 
 module "proxmox" {
   source = "./proxmox"
