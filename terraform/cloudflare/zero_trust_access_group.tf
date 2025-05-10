f98efd130452ac626c996a24aa5e111c1c6ec782
Mafyuh <admin@mafyuh.dev>
2025-05-10 00:35:28 -0400

    fix(various): misc fixes and updates

--- terraform/cloudflare/zero_trust_access_group.tf
+++ terraform/cloudflare/zero_trust_access_group.tf
@@ -2,6 +2,8 @@
 resource "cloudflare_zero_trust_access_group" "usa_access_group" {
   name    = "USA"
   account_id = data.bitwarden_secret.cf_account_id.value
+  exclude = []
+  require = []
   is_default = true
 
   include = [
