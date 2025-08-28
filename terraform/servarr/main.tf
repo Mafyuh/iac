618f4859ffaf74bd9d9ab292c75d614c2121df18
Mafyuh <admin@mafyuh.dev>
2025-08-27 23:52:24 -0400

    fix(terraform): update provider source paths to remove registry prefix

--- terraform/servarr/main.tf
+++ terraform/servarr/main.tf
@@ -16,15 +16,15 @@ terraform {
 
   required_providers {
     sonarr = {
-      source  = "registry.terraform.io/devopsarr/sonarr"
+      source  = "devopsarr/sonarr"
       version = "3.4.0"
     }
     prowlarr = {
-      source  = "registry.terraform.io/devopsarr/prowlarr"
+      source  = "devopsarr/prowlarr"
       version = "3.0.2"
     }
     radarr = {
-      source  = "registry.terraform.io/devopsarr/radarr"
+      source  = "devopsarr/radarr"
       version = "2.3.3"
     }
     bitwarden = {
