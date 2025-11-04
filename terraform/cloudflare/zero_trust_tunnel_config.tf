97649637d9fb99957ae2c4faf52aff99da831c3a
Mafyuh <admin@mafyuh.dev>
2025-11-04 15:49:42 -0500

    feat(zero_trust): add DNS record

--- terraform/cloudflare/zero_trust_tunnel_config.tf
+++ terraform/cloudflare/zero_trust_tunnel_config.tf
@@ -211,6 +211,31 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "main_tunnel" {
         }
       },
       {
+        hostname = "guess.${var.domains["io"]}"
+        service  = "http://10.69.69.2:3009"
+        path     = ""
+        origin_request = {
+          no_tls_verify            = false
+          origin_server_name       = ""
+          disable_chunked_encoding = false
+          http2_origin             = false
+          tcp_keep_alive           = 300
+          keep_alive_connections   = 100
+          keep_alive_timeout       = 90
+          tls_timeout              = 10
+          connect_timeout          = 30
+          http_host_header         = ""
+          proxy_type               = ""
+          ca_pool                  = ""
+          no_happy_eyeballs        = false
+          access = {
+            aud_tag   = [""]
+            team_name = "mafyuh"
+            required  = false
+          }
+        }
+      },
+      {
         hostname = "*.${var.domains["io"]}"
         service  = "http://10.0.0.215:80"
         path     = ""
