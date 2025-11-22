5e22e97d4ded51804b9736b18da946339d7ebe44
Mafyuh <admin@mafyuh.dev>
2025-11-22 18:59:32 -0500

    feat(envoy): init add envoy-authentik proxy setup

--- terraform/cloudflare/zero_trust_tunnel_config.tf
+++ terraform/cloudflare/zero_trust_tunnel_config.tf
@@ -236,6 +236,31 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "main_tunnel" {
         }
       },
       {
+        hostname = "bazarr.${var.domains["dev"]}"
+        service  = "https://10.0.0.217:443"
+        path     = ""
+        origin_request = {
+          no_tls_verify            = true
+          origin_server_name       = "bazarr.mafyuh.dev"
+          disable_chunked_encoding = false
+          http2_origin             = false
+          tcp_keep_alive           = 300
+          keep_alive_connections   = 100
+          keep_alive_timeout       = 90
+          tls_timeout              = 10
+          connect_timeout          = 30
+          http_host_header         = ""
+          proxy_type               = ""
+          ca_pool                  = ""
+          no_happy_eyeballs        = false
+          access = {
+            aud_tag   = [""]
+            team_name = "mafyuh"
+            required  = false
+          }
+        }
+      },
+      {
         hostname = "*.${var.domains["io"]}"
         service  = "http://10.0.0.217:80"
         path     = ""
