545b68cf7f9523c1b32219bf9c929daf240c9a06
Mafyuh <admin@mafyuh.dev>
2025-12-11 13:09:46 -0500

    fix(gatus): update service URLs for Radarr and Sonarr in gatus

--- terraform/cloudflare/zero_trust_tunnel_config.tf
+++ terraform/cloudflare/zero_trust_tunnel_config.tf
@@ -111,31 +111,6 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "main_tunnel" {
         }
       },
       {
-        hostname = "auth.${var.domains["io"]}"
-        service  = "https://10.0.0.217:443"
-        path     = ""
-        origin_request = {
-          no_tls_verify            = true
-          origin_server_name       = "auth.mafyuh.io"
-          disable_chunked_encoding = false
-          http2_origin             = false
-          tcp_keep_alive           = 300
-          keep_alive_connections   = 100
-          keep_alive_timeout       = 90
-          tls_timeout              = 10
-          connect_timeout          = 30
-          http_host_header         = ""
-          proxy_type               = ""
-          ca_pool                  = ""
-          no_happy_eyeballs        = false
-          access = {
-            aud_tag   = [""]
-            team_name = "mafyuh"
-            required  = false
-          }
-        }
-      },
-      {
         hostname = "prox.${var.domains["io"]}"
         service  = "https://10.0.0.30:8006"
         path     = ""
