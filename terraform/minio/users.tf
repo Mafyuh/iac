61b9e65444118dd444ebff6c48d44be011c52ece
Mafyuh <admin@mafyuh.dev>
2026-02-09 21:33:45 -0500

    chore(compose): replace minio with rustfs

--- terraform/minio/users.tf
+++
@@ -1,7 +0,0 @@
-resource "minio_iam_user" "terraform" {
-  name = "terraform"
-}
-
-resource "minio_iam_user" "postgres" {
-  name = "postgres"
-}
--- terraform/minio/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "access_token" {
-  description = "Bitwarden access token"
-  type        = string
-  sensitive   = true
-}
-
-data "bitwarden-secrets_secret" "minio_password" {
-  id = "aeb09725-39f5-47bf-823a-b31001738d02"
-}
