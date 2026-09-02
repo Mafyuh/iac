3c09d60b6c8bb34925d6b13996095ee5bce4365e
Mafyuh <matt@mafyuh.com>
2026-09-01 20:59:14 -0400

    fix(custom-init.sh): change permissions for 7zz

--- docker/downloaders/custom-init.sh
+++ docker/downloaders/custom-init.sh
@@ -2,5 +2,5 @@
 if [ -f "/usr/bin/7zz" ] && ! grep -q "exit 0" "/usr/bin/7zz" 2>/dev/null; then
   mv /usr/bin/7zz /usr/bin/7zz-real
   cp /custom-cont-init.d/7zz-wrapper /usr/bin/7zz
-  chmod +x /usr/bin/7zz
+  chmod 755 /usr/bin/7zz
 fi
